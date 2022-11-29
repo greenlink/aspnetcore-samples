4ed189c25607219fa939ddfa1aeb23ff6dd0c055
lennon.bravo <lennon.bravo@NTB-9YKVFR3>
2022-11-29 17:29:19 -0300

    add: covertype repositories class and interface.

---
+++ BulkyBook/BulkyBook.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository;
+
+public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+{
+    private readonly ApplicationDbContext _applicationDbContext;
+
+    public CoverTypeRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext) 
+        => _applicationDbContext = applicationDbContext;
+
+    public void Update(CoverType coverType) => _applicationDbContext.CoverTypes.Update(coverType);
+}
---
+++ BulkyBook/BulkyBook.DataAccess/Repository/IRepository/ICoverTypeRepository.cs
@@ -0,0 +1,7 @@
+﻿using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository.IRepository;
+public interface ICoverTypeRepository : IRepository<CoverType>
+{
+    void Update(CoverType coverType);
+}

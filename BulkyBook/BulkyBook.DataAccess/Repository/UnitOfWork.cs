cea5ecff3a75be85800861c7583c4373e487f18b
lennon.bravo <lennon.bravo@NTB-9YKVFR3>
2022-11-29 17:29:41 -0300

    modify: implement covertype to unit of work

--- BulkyBook/BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook/BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -6,11 +6,13 @@ public class UnitOfWork : IUnitOfWork
 {
     private readonly ApplicationDbContext _applicationDbContext;
     public ICategoryRepository CategoryRepository { get; private set; }
+    public ICoverTypeRepository CoverTypeRepository { get; private set; }
 
     public UnitOfWork(ApplicationDbContext applicationDbContext)
     {
         _applicationDbContext = applicationDbContext;
         CategoryRepository = new CategoryRepository(_applicationDbContext);
+        CoverTypeRepository = new CoverTypeRepository(_applicationDbContext);
     }
 
     public void Save() => _applicationDbContext.SaveChanges();

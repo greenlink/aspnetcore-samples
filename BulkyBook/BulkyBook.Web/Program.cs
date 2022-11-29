6d2e4414e8c22f64ac8cd6307a189051a5c9bad3
lennon.bravo <lennon.bravo@NTB-9YKVFR3>
2022-11-29 17:03:28 -0300

    modify: passing migration assembly for builder.

--- BulkyBook/BulkyBook.Web/Program.cs
+++ BulkyBook/BulkyBook.Web/Program.cs
@@ -8,8 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
-    ));
+    builder.Configuration.GetConnectionString("DefaultConnection"),
+    b => b.MigrationsAssembly("BulkyBook.DataAccess")));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 var app = builder.Build();

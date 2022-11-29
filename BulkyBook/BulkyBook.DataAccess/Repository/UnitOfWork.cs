4a541c254ecc9fbe0c6739762ab997394011962a
lennon.bravo <lennon.bravo@NTB-9YKVFR3>
2022-11-29 11:16:11 -0300

    add: unitofwork

---
+++ BulkyBook/BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+
+namespace BulkyBook.DataAccess.Repository;
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _applicationDbContext;
+    public ICategoryRepository CategoryRepository { get; private set; }
+
+    public UnitOfWork(ApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+        CategoryRepository = new CategoryRepository(_applicationDbContext);
+    }
+
+    public void Save() => _applicationDbContext.SaveChanges();
+}

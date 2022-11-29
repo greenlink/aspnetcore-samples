92861120b6d9f84298a75d55d3f4174e906944fb
lennon.bravo <lennon.bravo@NTB-9YKVFR3>
2022-11-29 10:41:42 -0300

    add: icategoryrepository.

---
+++ BulkyBook/BulkyBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository.IRepository;
+internal interface ICategoryRepository : IRepository<Category>
+{
+    void Update(Category category);
+    void Save();
+}

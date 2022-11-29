1015e4479e3424edbbd8ea79a588094fd3183548
lennon.bravo <lennon.bravo@NTB-9YKVFR3>
2022-11-29 10:46:53 -0300

    add: category repository.

---
+++ BulkyBook/BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository;
+public class CategoryRepository : Repository<Category>, ICategoryRepository
+{
+    private readonly ApplicationDbContext _applicationDbContext;
+
+    public CategoryRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext) 
+        => _applicationDbContext = applicationDbContext;
+
+    public void Save() => _applicationDbContext.SaveChanges();
+
+    public void Update(Category category) => _applicationDbContext.Categories.Update(category);
+}

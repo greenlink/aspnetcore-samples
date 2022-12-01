14f98c66e36bc1faea1700dfbcaabd1f5bd461e2
lennon.bravo <lennon.bravo@NTB-9YKVFR3>
2022-12-01 09:27:11 -0300

    modify: add display and maxlength annotation

--- BulkyBook/BulkyBook.Models/CoverType.cs
+++ BulkyBook/BulkyBook.Models/CoverType.cs
@@ -6,6 +6,8 @@ public class CoverType
 {
     [Key]
     public int Id { get; set; }
+    [Display(Name = "Cover Type")]
     [Required]
+    [MaxLength(50)]
     public string Name { get; set; }
 }

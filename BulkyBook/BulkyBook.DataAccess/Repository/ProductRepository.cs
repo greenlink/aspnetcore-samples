dcad9bb44ec68a140ffec4f0ff61b502ff9d605d
lennon.bravo <lennon.bravo@NTB-9YKVFR3>
2022-12-01 16:27:50 -0300

    add: product repository class and interface.

---
+++ BulkyBook/BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository;
+public class ProductRepository : Repository<Product>, IProductRepository
+{
+    private readonly ApplicationDbContext _applicationDbContext;
+
+    public ProductRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext) 
+        => _applicationDbContext = applicationDbContext;
+
+    public void Update(Product product)
+    {
+        var productFromDb = _applicationDbContext.Products.FirstOrDefault(p => p.Id == product.Id);
+        
+        if(productFromDb != null)
+        {
+            productFromDb.Title = product.Title;
+            productFromDb.Description = product.Description;
+            productFromDb.ISBN = product.ISBN;
+            productFromDb.Author = product.Author;
+            productFromDb.ListPrice = product.ListPrice;
+            productFromDb.Price = product.Price;
+            productFromDb.Price50 = product.Price50;
+            productFromDb.Price100 = product.Price100;
+            productFromDb.CategoryId = product.CategoryId;
+            productFromDb.CoverTypeId = product.CoverTypeId;
+
+            if(product.ImageUrl != null)
+                productFromDb.ImageUrl = product.ImageUrl;
+        }
+    }
+}

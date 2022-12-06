8c71f9b05d4049bc8c2dc3497c0c7f1b2d22b6fd
lennon.bravo <lennon.bravo@NTB-9YKVFR3>
2022-12-06 11:26:23 -0300

    add: savefile class to extract logic for dependency

---
+++ BulkyBook/BulkyBook.Utility/SaveFile/Interfaces/ISaveFile.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BulkyBook.Utility.SaveFile.Interfaces;
+public interface ISaveFile
+{
+    void Save(IFormFile formFile, string uploadPath);
+    string GetLastSavedFilePath();
+}
---
+++ BulkyBook/BulkyBook.Utility/SaveFile/SaveFile.cs
@@ -0,0 +1,28 @@
+﻿using BulkyBook.Utility.SaveFile.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace BulkyBook.Utility.SaveFile;
+public class SaveFile : ISaveFile
+{
+    private readonly IWebHostEnvironment _webHostEnvironment;
+    private string _lastSavedFilePath = string.Empty;
+
+    public SaveFile(IWebHostEnvironment webHostEnvironment) => _webHostEnvironment = webHostEnvironment;
+
+    public string GetLastSavedFilePath() => _lastSavedFilePath;
+
+    public void Save(IFormFile formFile, string uploadPath)
+    {
+        if (formFile == null) throw new ArgumentNullException(nameof(formFile));
+
+        var wwwRootPath = _webHostEnvironment.WebRootPath;
+        var fileName = Guid.NewGuid().ToString();
+        var uploads = Path.Combine(wwwRootPath, uploadPath);
+        var extension = Path.GetExtension(formFile.FileName);
+
+        using var filestream = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create);
+        formFile.CopyTo(filestream);
+        _lastSavedFilePath = uploadPath + @"\" + fileName + extension;
+    }
+}

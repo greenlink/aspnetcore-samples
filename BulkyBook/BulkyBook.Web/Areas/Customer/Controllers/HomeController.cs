003f81a694263ef3bc0f0c92bf204f6446dff8de
lennon.bravo <lennon.bravo@NTB-9YKVFR3>
2022-12-07 11:31:35 -0300

    add: shopping cart model

--- BulkyBook/BulkyBook.Web/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBook/BulkyBook.Web/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -20,6 +21,16 @@ public class HomeController : Controller
     {
         IEnumerable<Product> products = _unitOfWork.ProductRepository.GetAll("Category,CoverType");
         return View(products);
+    }
+
+    public IActionResult Details(int id)
+    {
+        var cart = new ShoppingCart
+        {
+            Product = _unitOfWork.ProductRepository.GetFirstOrDefault(p => p.Id == id, "Category,CoverType")
+        };
+
+        return View(cart);
     }
 
     public IActionResult Privacy()

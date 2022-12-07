5f9f106b99729b6024029ec7082c04e4f8a5fe80
lennon.bravo <lennon.bravo@NTB-9YKVFR3>
2022-12-07 11:24:46 -0300

    modify: some ajusts to design.

--- BulkyBook/BulkyBook.Web/wwwroot/js/product.js
+++ BulkyBook/BulkyBook.Web/wwwroot/js/product.js
@@ -11,7 +11,7 @@ function loadDataTable() {
                 "url": "/Admin/Product/GetAll"
             },
             "columns": [
-                { "data": "title", "width": "15%" },
+                { "data": "title", "width": "20%" },
                 { "data": "isbn", "width": "15%" },
                 { "data": "price", "width": "15%" },
                 { "data": "author", "width": "15%" },
@@ -20,10 +20,11 @@ function loadDataTable() {
                     "data": "id",
                     "render": function (data) {
                         return `<div class="w-75 btn-group" role="group">
-                                <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
+                                <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-primary mx-1"> <i class="bi bi-pencil-square"></i> Edit</a>
                                 <a onClick=Delete('/Admin/Product/Delete/${data}') class="btn btn-danger mx-1"><i class="bi bi-trash"></i> Delete</a>
                             </div>`;
-                    }
+                    },
+                    "width": "20%"
                 }
             ]
         });
